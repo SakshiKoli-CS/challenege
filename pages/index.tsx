ba974d772e8b7259aa35149a1eb85da2b8907ad3
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-07-21 16:50:56 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image'
+import { GetStaticProps } from 'next'
+
+type Movie = {
+  Title: string
+  Year: string
+  Runtime: string
+  Poster?: string
+}
+
+type HomeProps = {
+  movies: Movie[]
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const res = await fetch('https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies')
+  const movies: Movie[] = await res.json()
+
+  return {
+    props: {
+      movies,
+    },
+  }
+}
+
+export default function Home({ movies }: HomeProps) {
+  return (
+    <div>
+      <h1>Oswald</h1>
+
+      <div>
+        <Image
+          src="/OswaldCartoon.webp"
+          alt="Oswald"
+          width={300}
+          height={450}
+        />
+      </div>
+
+      <p>My Favorite Childhood Cartoon ❤️</p>
+
+      <h2>Movie List</h2>
+      <div>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            {movie.Poster ? (
+              <img src={movie.Poster} alt={movie.Title} />
+            ) : (
+              <div>No Image</div>
+            )}
+            <h3>{movie.Title}</h3>
+            <p>{movie.Year} • {movie.Runtime}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
