e3d1159764bcb53080339b8563d7cdf7a7902be1
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-07-23 14:44:36 +0530

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { GetStaticProps } from 'next'
 
@@ -10,36 +9,23 @@ type Movie = {
 }
 
 type HomeProps = {
-  movies: Movie[]
+  movie: Movie
 }
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const res = await fetch('https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies')
-  const movies: Movie[] = await res.json()
+  let movies: Movie[] = await res.json()
+
+  const movie = movies[Math.floor(Math.random() * movies.length)]
 
   return {
     props: {
-      movies,
+      movie,
     },
   }
 }
 
-function shuffleArray<T>(arr: T[]): T[] {
-  const shuffled = [...arr]
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
-  }
-  return shuffled
-}
-
-export default function Home({ movies }: HomeProps) {
-  const [shuffledMovies, setShuffledMovies] = useState<Movie[]>([])
-
-  useEffect(() => {
-    setShuffledMovies(shuffleArray(movies))
-  }, [movies])
-
+export default function Home({ movie }: HomeProps) {
   return (
     <div>
       <h1>Oswald</h1>
@@ -55,19 +41,15 @@ export default function Home({ movies }: HomeProps) {
 
       <p>My Favorite Childhood Cartoon ❤️</p>
 
-      <h2>Movie List</h2>
+      <h2>Featured Movie</h2>
       <div>
-        {shuffledMovies.map((movie, index) => (
-          <div key={index}>
-            {movie.Poster ? (
-              <img src={movie.Poster} alt={movie.Title} />
-            ) : (
-              <div>No Image</div>
-            )}
-            <h3>{movie.Title}</h3>
-            <p>{movie.Year} • {movie.Runtime}</p>
-          </div>
-        ))}
+        {movie.Poster ? (
+          <img src={movie.Poster} alt={movie.Title} />
+        ) : (
+          <div>No Image</div>
+        )}
+        <h3>{movie.Title}</h3>
+        <p>{movie.Year} • {movie.Runtime}</p>
       </div>
     </div>
   )
